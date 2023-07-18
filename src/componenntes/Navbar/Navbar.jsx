b456b4cf1576064a83fd521ea21fdec50d275405
vishal gupta <vishalguptadishu@gmail.com>
2023-07-18 22:19:10 +0530

    navbar

---
+++ src/componenntes/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css"
+
+const Navbar =()=>{
+    return (
+        <>
+            <nav className="Navbar">
+                <div className="logo">
+                  <img src="" alt="" />
+                </div>
+                <div className="Link_to_page">
+                  <p>Home</p>
+                  <p>jkdn</p>
+                  <p>,mdmcn</p>
+                  <p>,wdn</p>
+                </div>
+                <div className="serch" >
+                  <input type="text" placeholder="search" />
+                </div>
+                <div className="login" >
+                   <img src="" alt="" />
+                </div>
+            </nav>
+        
+        </>
+    )
+}
+export default Navbar;
